9221c10b02a07a71cd2055760d9bbe29dd8fd085
Oak Soe Htet <oaksoe.h@ku.th>
2023-11-15 17:12:58 +0700

    Added basic play functionality

--- Mastermind.py
+++ Mastermind.py
@@ -6,18 +6,36 @@ class Mastermind:
         self.colors = colors
         self.positions = positions
 
-        self.solution = []
+        self.solution = ''
         for i in range(positions):
-            self.solution.append(randint(1, colors))
+            self.solution += str(randint(1, colors))
 
         self.rounds = 0
 
     def play(self):
+        answer = ''
+
         print(f"Playing Mastermind with {self.colors} colors and {self.positions} positions")
-        guess = int(input("What is your guess?: "))
-        print(f'Your guess is {guess}')
-        
+
+        while answer != "*" * self.positions:
+            right_pos = ''
+            right_col = ''
+            guess = input("What is your guess?: ")
+            print(f'Your guess is {guess}')
+
+            for i in range(len(self.solution)):
+                if guess[i] == self.solution[i]:
+                    right_pos += '*'
+                elif guess[i] in self.solution:
+                    right_col += 'o'
+            answer = right_pos + right_col
+            self.rounds += 1
+            print(answer)
+            print()
+
+        print(f"You solve it after {self.rounds} rounds")
 
 
 game = Mastermind()
 print(game.solution)
+game.play()
